45090852de2de3d74efc2e2ae94958738a60cf0b
22051988Di <vitalii.soloninko@gmail.com>
2024-05-06 16:37:39 +0200

    v0.0.4	accept data from the form

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 // Telegraf - конструктор, Markup - для роботи з клавіатурою
 import { Markup, Telegraf } from 'telegraf'
+import { message } from 'telegraf/filters'
 
 const token = '7102467671:AAG7WDffMr_y52jK7fM9zex9lH4KbdnPGjA'
 const webAppUrl = 'https://angular-tg-app-a5dea.web.app'
@@ -12,12 +13,14 @@ bot.command('start', ctx => {
 	ctx.reply(
 		'Welcome. Click on the button to launch the application',
 		Markup.keyboard([
-			Markup.button.webApp(
-				'Send a message',
-				'https://angular-tg-app-a5dea.web.app/feedback'
-			),
+			Markup.button.webApp('Send a message', `${webAppUrl}/feedback`),
 		])
 	)
+})
+
+bot.on(message('web_app_data'), async ctx => {
+	const data = ctx.webAppData.data.json()
+	ctx.reply(`Your message: ${data?.feedback}` ?? 'Empty message')
 })
 
 bot.launch()
